36ef8f51103d9a7c3acb34e95c2d6de078feecff
Claman <alclaexman@gmail.com>
2013-11-21 21:47:16 -0600

    Almost all mechanics are now functional.

--- qu.py
+++ qu.py
@@ -1,42 +1,126 @@
 # qu.py
 # Alex Claman
 
+# room3 -- room4
+#   |        |
+#   |        |
+# room2 -- room1
+
 from rooms import *
+import sys
 
-# Room constructor is organized thusly: (north, east, south, west, room_name, room_desc, object_1, object_1_desc, object_2,
-# object_2_desc, object_3, object_3_desc, clue)
+# Room constructor is organized thusly: (room_name, room_desc, object_1,
+# object_1_desc, object_2, object_2_desc, object_3, object_3_desc, clue)
 # Room 1 has North and West entrances
-room1 = Room(True, False, False, True, 'Living Room', 'A generic living room', 'couch', 'This is just a couch. Get over it.', 'lamp', 'I love lamp', 'TV', 'Just playing some static', 'May all trouble begone with OxiClean.')
+room1 = Room('Living Room', 'A generic living room containing a couch,\
+  a lit lamp, and a TV.',
+  'couch', 'This is just a couch with a remote on it. Get over it.',
+  'lamp', 'It is casting a strange shadow on the wall.', 'TV',
+  'Just playing some static.',
+  'An infomercial: May all trouble begone with OxiClean.', 'use remote')
 # Room 2 has North and East entrances
-room2 = Room(True, True, False, False, 'Dining Room', 'There is a table and a cabinet. The floor is covered by a thick carpet.', 'table', 'A massive oak table.', 'cabinet', 'Contains cutlery, utensils, and plates.', 'carpet', 'An abnormally thick carpet. There might be something under it.', 'Everything is a lie. Except the cake. It is delicious.')
+room2 = Room('Dining Room', 'There is a table and a cabinet.\
+  The floor is covered by a thick carpet', 'table', 'A massive\
+  oak table.', 'cabinet','Contains cutlery, utensils, and plates.', 'carpet',
+  'An abnormally thick carpet. There might be something under it.', 'You\
+  find a old, yellowed letter with a key inside. It reads: Everything\
+  is a lie. Except the cake. It is delicious.', 'lift carpet')
 # Room 3 has South and East entrances
-room3 = Room(False, True, True, False, 'Parlor', 'There are several armchairs, a coffee table.', 'armchair', 'An exceptionally comfortable leather armchair.', 'coffee tabls', 'Has a few magazines scattered atop its surface.', 'china', 'Not the country, Einstein.', 'Avoid the tea. It is terrible today.')
+room3 = Room('Parlor', 'There are several armchairs and a coffee table.',
+  'armchair', 'An exceptionally comfortable leather armchair.',
+  'coffee table', 'Has some coffee cups and a few magazines scattered\
+  atop its surface.', 'coffee cups', 'Sadly, they do not contain coffee.',
+  'The headline reads: Avoid the tea. It\'s terrible today.', 'read magazine')
 # Room 4 has South and West elements
-room4 = Room(False, False, True, True, 'Bedroom', 'There is a bed and a wardrobe.', 'bed', 'A luxurious four-poster bed with something under it. It is not yours. Step away.', 'wardrobe', 'Contains a large number of old fur coats. May or may not contain a portal to Narnia.', 'something', 'A locked chest.', 'A diary page reading: cruncy peanut butter is better.')
+room4 = Room('Bedroom', 'There is a bed and a wardrobe.','bed',
+  'A luxurious four-poster bed with something under it. It is not yours.\
+  Step away.', 'wardrobe', 'Contains a large number of old fur coats.\
+  May or may not contain a portal to Narnia.', 'something',
+  'A locked chest.', 'The chest contains a diary page reading only:\
+  crunchy peanut butter is better.', 'unlock chest')
 
-room1.setConnectedRooms(room4)
-room1.setConnectedRooms(room2)
-room2.setConnectedRooms(room1)
-room2.setConnectedRooms(room3)
-room3.setConnectedRooms(room2)
-room3.setConnectedRooms(room4)
-room4.setConnectedRooms(room3)
-room4.setConnectedRooms(room1)
-room1Con = room1.getConnectedRooms()
-room2Con = room2.getConnectedRooms()
-room3Con = room3.getConnectedRooms()
-room4Con = room4.getConnectedRooms()
-print room1Con, room2Con, room3Con, room4Con
+# This next block sets the connections between rooms
+room1.setConnectedRoom(room4,'north')
+room1.setConnectedRoom(room2,'west')
+room2.setConnectedRoom(room1,'east')
+room2.setConnectedRoom(room3,'north')
+room3.setConnectedRoom(room2,'south')
+room3.setConnectedRoom(room4,'east')
+room4.setConnectedRoom(room3,'west')
+room4.setConnectedRoom(room1,'south')
 
-startRoom = room1
-currentRoom = room1
+room2.setItem('key')
 
-# def playerMove(move,room):
-  # if move in room.getDirections():
+def changeRoom(room):
+  current_room = room
 
-# def main():
-#   command = raw_input(">")
-#   if command == 'north' or command == 'south' or command == 'east' or command == 'west':
-#     playerMove(command, currentRoom)
+def playerMove(move,current_room):
+  if move in current_room.getExits():
+    connected_rooms = current_room.getConnectedRooms()
+    if move == 'north':
+      newRoom = current_room.north_room
+      return newRoom
+    elif move == 'south':
+      newRoom = current_room.south_room
+      return newRoom
+    elif move == 'east':
+      newRoom = current_room.east_room
+      return newRoom
+    elif move == 'west':
+      newRoom = current_room.west_room
+      return newRoom
+  else:
+    print 'You can\'t move in that direction.'
 
-# main()
+def addToBackpack(item):
+  backpack.append(item)
+
+def exitGame():
+  sys.exit('Exiting Game...')
+
+# if 'key' in backpack:
+#   unlock_chest
+# else:
+#   'the chest is locked'
+
+def main():
+  current_room = room1
+  backpack = []
+  print 'Move using north, south, east, and west. Explore. Investigate. Escape.'
+  while True:
+    command = raw_input(">")
+    if command in ('north', 'south', 'east', 'west'):
+      current_room = playerMove(command,current_room)
+      info = current_room.getRoomInfo()
+      print info
+    elif command == 'look':
+      info = current_room.getRoomInfo()
+      print info
+    elif len(command) > 8 and command[0:7] == 'examine':
+      entries = command.split(' ')
+      atwas = entries[1]
+      if atwas in current_room.listObjects():
+        object_info = current_room.getObjectDescription(atwas)
+        print object_info
+      else:
+        print 'I don\'t know what that is.'
+    elif command[0:2] == 'get':
+      entries = command.split(' ')
+      item = entries[1]
+      if item == current_room.item:
+        addToBackpack(item)
+        item,'was added to your backpack.'
+      else:
+        print 'I can\'t do that.'
+    elif command == 'backpack':
+      if backpack != []:
+        print backpack
+      else:
+        print 'Your backpack is empty.'
+    elif command == current_room.getClueAction():
+      print current_room.clue
+    elif command in ('exit','quit','q'):
+      exitGame()
+    else:
+      print 'I don\'t know what that means.'
+main()
--- rooms.py
+++ rooms.py
@@ -2,8 +2,8 @@
 # code by Alex Claman
 
 class Room():
-  def __init__(self, north, east, south, west, room_name, room_desc, object_1, object_1_desc,
-              object_2, object_2_desc, object_3, object_3_desc, clue):
+  def __init__(self, room_name, room_desc, object_1, object_1_desc,
+              object_2, object_2_desc, object_3, object_3_desc, clue, clue_action):
     self.room_name = room_name
     self.room_desc = room_desc
     self.object_1 = object_1
@@ -13,38 +13,68 @@ class Room():
     self.object_3 = object_3
     self.object_3_desc = object_3_desc
     self.clue = clue
+    self.clue_action = clue_action
     self.exits = []
-    if north == True:
-      self.exits.append('North')
-    if east == True:
-      self.exits.append('East')
-    if south == True:
-      self.exits.append('South')
-    if west == True:
-      self.exits.append('West')
     self.connectedRooms = []
+    self.north_room = None
+    self.south_room = None
+    self.east_room = None
+    self.west_room = None
+    self.item = None
 
   def getExits(self):
     return self.exits
 
-  def setConnectedRooms(self,room):
-    self.connectedRooms.append(room.room_name)
+  def setConnectedRoom(self,room,direction):
+    if direction == 'north':
+      self.north_room = room
+      self.connectedRooms.append(room)
+      self.exits.append(direction)
+    elif direction == 'south':
+      self.south_room = room
+      self.connectedRooms.append(room)
+      self.exits.append(direction)
+    elif direction == 'east':
+      self.east_room = room
+      self.connectedRooms.append(room)
+      self.exits.append(direction)
+    elif direction == 'west':
+      self.west_room = room
+      self.connectedRooms.append(room)
+      self.exits.append(direction)
 
   def getConnectedRooms(self):
     return self.connectedRooms
 
   def getRoomInfo(self):
-    return self.room_name, self.room_desc
+    return 'This is the '+ self.room_name + '. ' + self.room_desc + '.'
 
-  def getObjects(self):
-    return self.object_1, self.object_1_desc, self.object_2, self.object_2_desc, self.object_3, self.object_3_desc
+  def listObjects(self):
+    return self.object_1, self.object_2, self.object_3
+
+  # atwas is object in German. I'm using atwas because object is a reserved word.
+  def getObjectDescription(self, atwas):
+    if atwas == self.object_1:
+      return self.object_1_desc
+    elif atwas == self.object_2:
+      return self.object_2_desc
+    elif atwas == self.object_3:
+      return self.object_3_desc
+
+  def getClueAction(self):
+    return self.clue_action
 
   def getClue(self):
     return self.clue
 
+  def setItem(self,item):
+    self.item = item
+
 def main():
-  room1 = Room(True, False, False, True, 'Living Room', 'A generic living room', 'couch', 'This is just a couch. Get over it.',
-    'lamp', 'I love lamp', 'TV', 'Just playing some static', 'May all trouble begone with OxiClean.')
+  room1 = Room(True, False, False, True, 'Living Room', 'A generic living room',
+    'couch', 'This is just a couch. Get over it.', 'lamp', 'I love lamp',
+    'TV', 'Just playing some static', 'May all trouble begone with OxiClean.',
+    'clean couch.')
   print room1.getExits()
   print room1.getRoomInfo()
   room1Objects = room1.getObjects()
@@ -53,9 +83,12 @@ def main():
   print room1Objects[4]+ ':', room1Objects[5]
   print room1.getClue()
 
-  room2 = Room(False, True, True, True, 'Dining Room', 'There is a table and a cabinet. The floor is covered by a thick carpet.',
-    'table', 'A massive oak table.', 'cabinet', 'Contains cutlery, utensils, and plates.', 'carpet',
-    'An abnormally thick carpet. There might be something under it.', 'Everything is a lie. Except the cake.')
+  room2 = Room(False, True, True, True, 'Dining Room',
+    'There is a table and a cabinet. The floor is covered by a thick carpet.',
+    'table', 'A massive oak table.', 'cabinet',
+    'Contains cutlery, utensils, and plates.', 'carpet',
+    'An abnormally thick carpet. There might be something under it.',
+    'Everything is a lie. Except the cake.', 'Break GLaDOS.')
   print room2.getExits()
 
 if __name__ == "__main__":
